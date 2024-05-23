3c17288fb85fb230f5a4850704b729b9aa016b8d
tkykkkkk <158381693+tkykkkkk@users.noreply.github.com>
2024-05-23 04:56:48 +0000

    DM機能追加完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
 #   アソシエーションゾーン
 has_many :post_comments, dependent: :destroy
-has_many :post_tags
-has_many :favorites
+has_many :post_tags, dependent: :destroy
+has_many :favorites, dependent: :destroy
 belongs_to :user
 has_many :photos, dependent: :destroy
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  #through: :posts
-  
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+  has_many :rooms, through: :entries, dependent: :destroy
   # フォローしている関連付け
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # フォローされている関連付け
